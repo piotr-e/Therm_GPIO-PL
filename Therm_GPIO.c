2e27728aea30b0b92515db041ff5bb8e37de62d4
All for Raspberry Pi. <piotr-e@wp.pl>
2013-03-16 20:30:23 +0100

    Update Therm_GPIO.c
    programing

--- Therm_GPIO.c
+++ Therm_GPIO.c
@@ -20,3 +20,42 @@
 int g;  //Zmienna pinu GPIO.
 char buf[8];  //Zmienna potrzebna do konwersji temperatury.
 unsigned char inicjalizacja(int pin);
+
+int main(int argc, char **argv)
+{
+     if (!bcm2835_init())
+          return 1;
+//    [tutaj znajdzie się kod programu...]
+
+    bcm2835_close();
+}
+
+unsigned char inicjalizacja(int pin) 
+{
+    bcm2835_gpio_fsel(pin, BCM2835_GPIO_FSEL_OUTP); //Ustawienie pinu jako wyjscie.
+    STAN_NISKI(pin);  //Ustawienie magistrali w stan niski.
+    delay(500);  //Odczekanie 500 milisekund.
+    STAN_WYSOKI(pin);  //Zwolnienie magistrali.
+    bcm2835_gpio_fsel(pin, BCM2835_GPIO_FSEL_INPT);  //Ustawienie pinu jako wejscie.
+    bcm2835_gpio_set_pud(pin, BCM2835_GPIO_PUD_UP);
+
+    int stat;
+    stat = bcm2835_gpio_lev(pin);
+    if (stat==0) {
+        printf("DS18B20 zglosil swoja obecnosc.\n");
+        }
+        else
+        {
+        printf("Brak DS18B20 lub blad w podlaczeniu.\n");
+        }
+    stat = bcm2835_gpio_lev(pin);
+    if (stat==0){
+       printf("Nadal stan niski.\n");
+       }
+       else
+       {
+       printf("Magistrala gotowa do komunikacji.\n");
+       }
+    return 0;
+}
+
